97674c327cd5318578bb5301109b085e0dc9bfab
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-06-14 09:48:12 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" href="assets/css/style.css" />
+</head>
+<body>
+<div class="center">
+	Weekly Specials
+	<hr>
+	<iframe src="https://secure.vacationport.net/TravelInfo/Group?host=archertravel.vacationport.net&PriceGroupId=4229"></iframe>
+	
+	<!-- TODO 2: Create custom ad space for paid supplier specials/banners -->
+</div>
+</body>
+</html>
