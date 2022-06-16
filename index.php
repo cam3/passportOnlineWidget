3ccc0d597954ae901f2dcf1249204016597797d1
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-06-16 11:07:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,10 @@
 <link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
-<div class="center">
-	Weekly Specials
-	<hr>
-	<!-- TODO 1: Get each 'card' outside of iframe, or in independent frame for better design/flow -->
-	<iframe src="https://archertravel.vacationport.net/TravelInfo/Group?PriceGroupId=4229"></iframe>
-	
-	<!-- TODO 2: Create custom ad space for paid supplier specials/banners -->
-</div>
+	<div class="center">
+		<p>Weekly Specials</p>
+		<hr>
+		<iframe src="https://archertravel.vacationport.net/TravelInfo/Group?PriceGroupId=4229"></iframe>
+	</div>
 </body>
 </html>
