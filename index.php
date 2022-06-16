9ef4f3a2fed18fee29c4102f3ed37a7e328c870c
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2022-06-16 12:23:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
 <link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
-	<a href="http://www.latesttraveloffers.com/xmlout/GetSSXML.asp?site=VTHARC&OfferGroupID=18181">passportOnlineAPI</a>
+  <?php
+
+  $apiKey = "VTHARC";
+  $offerGroupID = "18181";
+  $url = "http://www.latesttraveloffers.com/xmlout/GetSSXML.asp?site=$apiKey&OfferGroupID=$offerGroupID";
+
+  $xml = file_get_contents($url);
+  $xmlout = simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA);
+  $json = json_encode($xmlout);
+  $array = json_decode($json,TRUE);
+
+  echo $array;
+
+  ?>
 </body>
 </html>
